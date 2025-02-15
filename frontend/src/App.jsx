79bb97c88c2f3f38146bd41a9f451f106bbcbecb
user.name:"Rishabh Tiwari <user.email:"rishabhtiwari248612@gmail.com>
2025-02-15 13:19:02 +0530

    installing shadcn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
+import { Button } from './components/ui/button';
+
 function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600">
         Tailwind CSS with Vite! 🚀
       </h1>
+      <Button>Click me</Button>
     </div>
   );
 }
